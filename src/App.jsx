d21d0018632040f96210badfd1d402289a24399d
Shreya Ghosh <sgshreya024@gmail.com>
2022-04-23 09:07:34 +0530

    Dashboard CSS Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,95 +1,206 @@
 // Dependency & Library
-import './App.css'
-import React, { useState, useEffect } from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 // Routes & Components
-import ProtectedRoute from './authorize/ProtectedRoute'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
+import ProtectedRoute from "./authorize/ProtectedRoute";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 // Page Components
-import Home from './pages/Home'
-import Login from './pages/Login'
-import Logout from './pages/Dashboard/Logout'
-import Dashboard from './pages/Dashboard/Dashboard'
-import NotFound from './pages/NotFound'
-import Block from './pages/Dashboard/Block/Block'
-import RoomBlock from './pages/Dashboard/Block/RoomBlock'
-import Staff from './pages/Dashboard/Staff/Staff'
-import AddStaff from './pages/Dashboard/Staff/AddStaff'
-import Rooms from './pages/Dashboard/Room/Rooms'
-import RoomDetail from './pages/Dashboard/Room/RoomDetail'
-import AddRoom from './pages/Dashboard/Room/AddRoom'
-import Student from './pages/Dashboard/Student/Student'
-import AddStudent from './pages/Dashboard/Student/AddStudent'
-import StudentDetail from './pages/Dashboard/Student/StudentDetail'
-
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Logout from "./pages/Dashboard/Logout";
+import Dashboard from "./pages/Dashboard/Dashboard";
+import NotFound from "./pages/NotFound";
+import Block from "./pages/Dashboard/Block/Block";
+import RoomBlock from "./pages/Dashboard/Block/RoomBlock";
+import Staff from "./pages/Dashboard/Staff/Staff";
+import AddStaff from "./pages/Dashboard/Staff/AddStaff";
+import Rooms from "./pages/Dashboard/Room/Rooms";
+import RoomDetail from "./pages/Dashboard/Room/RoomDetail";
+import AddRoom from "./pages/Dashboard/Room/AddRoom";
+import Student from "./pages/Dashboard/Student/Student";
+import AddStudent from "./pages/Dashboard/Student/AddStudent";
+import StudentDetail from "./pages/Dashboard/Student/StudentDetail";
 
 function App() {
+  const [state, setState] = useState();
+  const [apiLink, setApiLink] = useState(
+    "https://hostel-management-omega.vercel.app"
+  );
 
-  const [state, setState] = useState()
-  const [apiLink, setApiLink] = useState('https://hostel-management-omega.vercel.app')
-  
   useEffect(() => {
-    const local =  JSON.parse(localStorage.getItem('state'))
-    setState(local)
+    const local = JSON.parse(localStorage.getItem("state"));
+    setState(local);
 
     //token lost on refresh
     // localStorage.removeItem("auth-token")
-  }, [])
+  }, []);
 
   return (
     <div className="App">
-        <Router basename="hostel-management" >
-          <Navbar state={state} />
+      <Router basename="hostel-management">
+        <Navbar state={state} />
 
-          <div className='content'>
-            <Routes >
-              <Route exact path="/" element={<Home/>} />
+        <div className="content">
+          <Routes>
+            <Route exact path="/" element={<Home />} />
 
-              <Route path="/login" element={<ProtectedRoute status='loggedOut' state={state} apiLink={apiLink} />} >
-                <Route index element={<Login state={state} apiLink={apiLink} setState={setState} />} />
+            <Route
+              path="/login"
+              element={
+                <ProtectedRoute
+                  status="loggedOut"
+                  state={state}
+                  apiLink={apiLink}
+                />
+              }
+            >
+              <Route
+                index
+                element={
+                  <Login state={state} apiLink={apiLink} setState={setState} />
+                }
+              />
+            </Route>
+
+            <Route
+              path="/dashboard"
+              element={
+                <ProtectedRoute
+                  status="loggedIn"
+                  state={state}
+                  apiLink={apiLink}
+                />
+              }
+            >
+              <Route
+                index
+                element={<Dashboard state={state} apiLink={apiLink} />}
+              />
+
+              <Route path="staff">
+                <Route
+                  index
+                  element={
+                    <Staff
+                      state={state}
+                      apiLink={apiLink}
+                      setState={setState}
+                    />
+                  }
+                />
+                <Route
+                  path="add"
+                  element={
+                    <AddStaff
+                      state={state}
+                      apiLink={apiLink}
+                      setState={setState}
+                    />
+                  }
+                />
               </Route>
 
-              <Route path="/dashboard" element={<ProtectedRoute status='loggedIn' state={state} apiLink={apiLink} />}>
-                <Route index element={<Dashboard state={state} apiLink={apiLink} />} />
-
-                <Route path="staff">
-                  <Route index element={ <Staff state={state} apiLink={apiLink} setState={setState}/> } />
-                  <Route path="add" element={ <AddStaff state={state} apiLink={apiLink} setState={setState} /> } />
-                </Route >
-
-
-                <Route path="block">
-                  <Route index element={ <Block state={state} apiLink={apiLink} /> } />
-                  <Route path=":blockId" element={ <RoomBlock state={state} apiLink={apiLink} /> } />
-                </Route>
-
-                <Route path="student">
-                  <Route index element={ <Student state={state} apiLink={apiLink} setState={setState}/> } />
-                  <Route path="add" element={ <AddStudent state={state} apiLink={apiLink} setState={setState}/> } />
-                  <Route path=":studentRoll" element={ <StudentDetail state={state} apiLink={apiLink} setState={setState}/> } />
-                </Route>
-
-                <Route path="room">
-                  <Route index element={ <Rooms state={state} apiLink={apiLink} setState={setState}/> } />
-                  <Route path="add" element={ <AddRoom state={state} apiLink={apiLink} setState={setState}/> } />
-                  <Route path=":roomId" element={ <RoomDetail state={state} apiLink={apiLink} setState={setState}/>  } />
-                </Route>
-
+              <Route path="block">
+                <Route
+                  index
+                  element={<Block state={state} apiLink={apiLink} />}
+                />
+                <Route
+                  path=":blockId"
+                  element={<RoomBlock state={state} apiLink={apiLink} />}
+                />
               </Route>
 
-              <Route path="/logout" element={<ProtectedRoute status='loggedIn' state={state} apiLink={apiLink} />}>
-                <Route index element={<Logout setState={setState}/>} />
+              <Route path="student">
+                <Route
+                  index
+                  element={
+                    <Student
+                      state={state}
+                      apiLink={apiLink}
+                      setState={setState}
+                    />
+                  }
+                />
+                <Route
+                  path="add"
+                  element={
+                    <AddStudent
+                      state={state}
+                      apiLink={apiLink}
+                      setState={setState}
+                    />
+                  }
+                />
+                <Route
+                  path=":studentRoll"
+                  element={
+                    <StudentDetail
+                      state={state}
+                      apiLink={apiLink}
+                      setState={setState}
+                    />
+                  }
+                />
               </Route>
 
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </div>
+              <Route path="room">
+                <Route
+                  index
+                  element={
+                    <Rooms
+                      state={state}
+                      apiLink={apiLink}
+                      setState={setState}
+                    />
+                  }
+                />
+                <Route
+                  path="add"
+                  element={
+                    <AddRoom
+                      state={state}
+                      apiLink={apiLink}
+                      setState={setState}
+                    />
+                  }
+                />
+                <Route
+                  path=":roomId"
+                  element={
+                    <RoomDetail
+                      state={state}
+                      apiLink={apiLink}
+                      setState={setState}
+                    />
+                  }
+                />
+              </Route>
+            </Route>
 
-          <Footer />
-        </Router>
+            <Route
+              path="/logout"
+              element={
+                <ProtectedRoute
+                  status="loggedIn"
+                  state={state}
+                  apiLink={apiLink}
+                />
+              }
+            >
+              <Route index element={<Logout setState={setState} />} />
+            </Route>
+
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </div>
+
+        <Footer />
+      </Router>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,45 +1,89 @@
-import React from 'react';
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
+import dashboard_classes from "./Dashboard.module.css";
+import student from "../../images/student.png";
+import staff from "../../images/staff.png";
+import room from "../../images/room.png";
 
 const Dashboard = (props) => {
+  function getBlock(id) {
+    return props.state.static.block.filter((x) => x.id === id)[0];
+  }
 
-    function getBlock(id){
-        return props.state.static.block.filter(x => x.id === id)[0]
-    }
+  return (
+    <div>
+      <h1 className={dashboard_classes.welcome}>
+        WELCOME {props.state.self.name}
+      </h1>
+      {/*
+      <ul>
+        <li>Name: {props.state && props.state.self.name}</li>
+        <li>Role: {props.state && props.state.self.role}</li>
+        <li>EmailID: {props.state && props.state.self.email}</li>
+  </ul>*/}
 
-    return(
-        <div>
-            
-            <h1>Dashboard</h1>
-            <ul>
-                <li>Name: { props.state && props.state.self.name }</li>
-                <li>Role: { props.state && props.state.self.role }</li>
-                <li>EmailID: { props.state && props.state.self.email }</li>
-            </ul>
+      <hr />
+      <div className={dashboard_classes.grid_container}>
+        <div className={dashboard_classes.grid_item}>
+          <img src={student} alt="student" />
 
-            <hr />
-            
-            <h4>
-                Block Access: { props.state && (props.state.block_access.length === 0)?<span className="badge bg-danger">No access</span>:
-                    props.state.block_access.map((x, index) => <span key={index}><Link to={`block/${x}`} className="btn btn-primary me-2" >{getBlock(x).name}</Link></span>)
-                }
-            </h4>
-
-            <hr />
-            <h4> Room Access</h4>
-                <Link to="room" className="btn btn-info mx-2 my-1">Room View</Link>
-            <hr />
-            <h4>Staff Access</h4>
-                <Link to="staff" className="btn btn-info mx-2 my-1">Manage Staff</Link>
-            <hr />
-            <h4>Student Access</h4>
-            <Link to="student" className="btn btn-info mx-2 my-1">Student View</Link>
-            <hr />
-            <h4>Block Access</h4>
-            <Link to="block" className="btn btn-info mx-2 my-1">Block View</Link>
-
+          <Link
+            to="student"
+            className={[dashboard_classes.grid_button, "mx-2 my-1"].join(" ")}
+          >
+            STUDENT
+          </Link>
         </div>
-    )
-}
+        <div className={dashboard_classes.grid_item}>
+          <img src={room} alt="room" />
 
-export default Dashboard
+          <Link
+            to="room"
+            className={[dashboard_classes.grid_button, "mx-2 my-1"].join(" ")}
+          >
+            ROOM STATUS
+          </Link>
+        </div>
+        <div className={dashboard_classes.grid_item}>
+          <img src={staff} alt="staff" />
+
+          <Link
+            to="staff"
+            className={[dashboard_classes.grid_button, "mx-2 my-1"].join(" ")}
+          >
+            STAFF
+          </Link>
+        </div>
+      </div>
+
+      <hr />
+
+      <Link
+        to="block"
+        className={[dashboard_classes.grid_button, "mx-2 my-1"].join(" ")}
+      >
+        Block View
+      </Link>
+      <h4>
+        {props.state && props.state.block_access.length === 0 ? (
+          <span className="badge bg-danger">No access</span>
+        ) : (
+          props.state.block_access.map((x, index) => (
+            <span key={index}>
+              <Link
+                to={`block/${x}`}
+                className={[dashboard_classes.button, "mx-2"].join(" ")}
+              >
+                {getBlock(x).name}
+              </Link>
+            </span>
+          ))
+        )}
+      </h4>
+
+      <hr />
+    </div>
+  );
+};
+
+export default Dashboard;
